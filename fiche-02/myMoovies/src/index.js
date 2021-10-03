f9c7951f5b86fd422aa93a56bb49eb3e21093d37
Sacha Norden <sacha.norden@student.vinci.be>
2021-10-03 20:49:08 +0200

    picture add to web

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -2,6 +2,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
-
+import Pizza from "./img/Pizza.jpg"; 
+import spinoza from "./img/spinoza.jpg";
 // This is the entry point to your app : add all relevant import and custom code
-
+let pizzaimg =document.getElementById("pizza");
+let spinoz =document.getElementById("spinoza");
+pizzaimg.src =Pizza;
+spinoz.src = spinoza;
