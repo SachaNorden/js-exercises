41a46445f2d17033c2da53cef1c5b57569e02a62
SachaNorden <90783180+SachaNorden@users.noreply.github.com>
2021-10-04 10:28:18 +0200

    table in progress

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -7,5 +7,19 @@ import spinoza from "./img/spinoza.jpg";
 // This is the entry point to your app : add all relevant import and custom code
 let pizzaimg =document.getElementById("pizza");
 let spinoz =document.getElementById("spinoza");
+let lines=document.forms["form"]["lines"].value;
+let column=document.forms["form"]["column"].value;
+let string=document.forms["form"]["string"].value;
 pizzaimg.src =Pizza;
 spinoz.src = spinoza;
+function table(){
+    for(let i=0;i<lines;i++){
+        for(let j=0;j<column;j++){
+            let text=string+" [ "+i+" ] [ "+j+" ]";
+            alert("yo");
+        }
+    }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", table);
