efdbe6ea3c0f52fd3da507f723ae50da15602f3d
SachaNorden <sacha.norden@student.vinci.be>
2021-10-07 11:45:31 +0200

    success table show

--- fiche-02/myMoovies/src/index.js
+++ fiche-02/myMoovies/src/index.js
@@ -1,5 +1,5 @@
 // Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
 import Pizza from "./img/Pizza.jpg"; 
@@ -7,19 +7,39 @@ import spinoza from "./img/spinoza.jpg";
 // This is the entry point to your app : add all relevant import and custom code
 let pizzaimg =document.getElementById("pizza");
 let spinoz =document.getElementById("spinoza");
-let lines=document.forms["form"]["lines"].value;
-let column=document.forms["form"]["column"].value;
-let string=document.forms["form"]["string"].value;
+
 pizzaimg.src =Pizza;
 spinoz.src = spinoza;
-function table(){
-    for(let i=0;i<lines;i++){
-        for(let j=0;j<column;j++){
-            let text=string+" [ "+i+" ] [ "+j+" ]";
-            alert("yo");
-        }
-    }
-}
-
 const form = document.querySelector("form");
 form.addEventListener("submit", table);
+
+function table(e){
+    e.preventDefault();
+    let lines=document.forms["form"]["lines"].value;
+    let column=document.forms["form"]["column"].value;
+    let string=document.forms["form"]["string"].value;
+    if(isNaN(lines)){
+        alert("Veuillez entrez un nombre valide svp");
+        return false;
+    }
+    if(isNaN(column)){
+        alert("Veuillez entrez un nombre valide svp");
+        return false;
+    }
+    let main =document.querySelector("main");
+    let table =document.createElement("table");
+    main.appendChild(table);
+    table.setAttribute('class','border border-secondary');
+
+    for(let i=0;i<lines;i++){
+        let listRow = document.createElement("tr");
+        for(let j=0;j<column;j++){
+            let listCol = document.createElement("td");
+            listCol.setAttribute('class','border border-primary m-2 p-3');
+            let text=document.createTextNode( string+" [ "+i+" ] [ "+j+" ]");
+            listCol.appendChild(text);
+            listRow.appendChild(listCol);
+        }
+        table.appendChild(listRow);
+    }
+}
