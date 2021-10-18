74e3b0598e03d7d8a4a1b86efb1a22136679b452
Sacha Norden <sacha.norden@student.vinci.be>
2021-10-18 10:32:48 +0200

    film min duration

--- fiche-04/myMoovies/routes/films.js
+++ fiche-04/myMoovies/routes/films.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const FILMS = [
-  { id: 1, title: "Spiderman", duration: 121, budget: 139000000 },
-  { id: 2, title: "Thor", duration: 114, budget:150000000 },
-  { id: 2, title: "Avengers", duration:143 , budget: 220000000 },
+  { id: 1, title: "Spiderman", duration: 121, budget: 139000000,link:"https://fr.wikipedia.org/wiki/Spider-Man_(s%C3%A9rie_de_films)" },
+  { id: 2, title: "Thor", duration: 114, budget:150000000 ,link:"https://fr.wikipedia.org/wiki/Thor"},
+  { id: 3, title: "Avengers", duration:143 , budget: 220000000 ,link:"https://fr.wikipedia.org/wiki/Avengers_(film)"},
 ];
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   return res.json(FILMS);
 });
+
 router.post("/", function (req, res, next) {
   if (!req.body || !req.body.title || !req.body.duration || !req.body.budget)
     return res.sendStatus(400);
